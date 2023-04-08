402a4c90e9f26ab0d4a5b9f83c1c5a66defc18f6
PocoPota <pocopota.program@gmail.com>
2023-04-08 22:27:22 +0900

    Create PracticeA.cpp

---
+++ AtCoder-Beginners-Selection/PracticeA.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int a;
+	int b, c;
+	string s;
+ 
+	cin >> a;
+	cin >> b >> c;
+	cin >> s;
+ 
+	int sum = a + b + c;
+	cout << sum << " " << s;
+ 
+	return 0;
+}
